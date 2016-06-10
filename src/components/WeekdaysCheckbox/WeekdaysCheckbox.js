673b95e4eec3e1419598f75ec6dee7535b965ffb
Tianyun Xu <mxu37@smith.edu>
2016-06-10 15:23:15 +0900

    checkboxs

---
+++ src/components/WeekdaysCheckbox/WeekdaysCheckbox.js
@@ -0,0 +1,12 @@
+import template from './WeekdaysCheckbox.html'
+import Vue from 'vue'
+
+export default Vue.extend({
+  template: template,
+
+  data () {
+    return {
+      checked: []
+    }
+  }
+})
